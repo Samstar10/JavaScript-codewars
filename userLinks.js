94dd1828c6c979a120c50a33ad0b2c791af9616b
Samuel Muli <samuelmuli95@gmail.com>
2023-12-27 22:41:00 +0300

    Generate User Link 8kyu codewars

--- userLinks.js
+++ userLinks.js
@@ -5,4 +5,10 @@
 // generate_link('matt c')
 // http://www.codewars.com/users/matt%20c
 
-// 
+// SOLUTION
+
+function generateLink(user) {
+    const sanitizedUsername = encodeURIComponent(user.trim())
+    const userLink = `http://www.codewars.com/users/${sanitizedUsername}`
+    return userLink
+}
